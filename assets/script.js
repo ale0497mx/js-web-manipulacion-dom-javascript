7df5557da04263b15c5f50eafa451dd43b166265
Aleandro_Resendiz <ale_resendiz321@hotmail.com>
2023-07-12 15:28:19 -0600

    clase05 completada

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-const btnAdd = document.querySelector('[data-form-btn]');
-console.log(btnAdd);
+( () => {
+    const btnAdd = document.querySelector('[data-form-btn]');
 //listener esto lo necitamos cuando le demos click el boton escuche o sepa que se dio 
 // el evento addEventListener es una forma de cvrear esa funcion
 
@@ -11,22 +11,36 @@ const createTasks = (evento) =>{
     const task = document.createElement('li');
     task.classList.add('card')
     input.value = "";
+    //backticks
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
     const content = ` 
-    <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-
-    console.log(content);
 }
 
-console.log(btnAdd);
 
 btnAdd.addEventListener('click', createTasks);
 
+const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+}
+//Immediantly invoked funtion expression IIFE  son funciones que en cuanto se declaran se ejecutan
+const completeTask = () => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+}
 
 //Arrow funtions o funciones flechas va,os a generar esa fucnion en una funcion flecha
 // btnAdd.addEventListener("click", (evento) => {
@@ -36,3 +50,4 @@ btnAdd.addEventListener('click', createTasks);
 //     console.log(input.value);
 //     // console.log("crear tarea");
 // });
+})();
